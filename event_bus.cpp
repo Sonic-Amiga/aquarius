91c10fd6caa8005032666b81445c78d3dec680d4
Pavel Fedin <pavel_fedin@mail.ru>
2021-12-01 23:09:06 +0300

    event_bus: Store last values
    Implement basic generic value (GValue) container and store all the values in a
    map for possible retrieval on demand by listeners. Events on duplicated values
    are not generated.

--- event_bus.cpp
+++ event_bus.cpp
@@ -1,12 +1,44 @@
+#include <map>
+#include <variant>
+
 #include "event_bus.h"
 #include "logging.h"
 
+typedef std::variant<int, float> GValue;
+
+std::ostream& operator<<(std::ostream& os, GValue v)
+{
+    if (std::holds_alternative<int>(v))
+        os << "<int>" << std::get<int>(v);
+    else if (std::holds_alternative<float>(v))
+        os << "<float>" << std::get<float>(v);
+    else
+        os << "<UNKNOWN TYPE>";
+
+    return os;
+}
+
+std::map<std::string, GValue> g_values;
+
+void SendEvent(const std::string& topic, GValue value)
+{
+    auto it = g_values.find(topic);
+
+    if (it != g_values.end() && it->second == value) {
+        // Nothing has changed, don't bother listeners
+        return;
+    }
+
+    g_values[topic] = value;
+    Log(Log::DEBUG) << topic << " = " << value;
+}
+
 void SendEvent(const std::string& topic, int value)
 {
-    Log(Log::DEBUG) << topic << " = " << value;
+    SendEvent(topic, GValue(value));
 }
 
 void SendEvent(const std::string& topic, float value)
 {
-    Log(Log::DEBUG) << topic << " = " << value;
+    SendEvent(topic, GValue(value));
 }
